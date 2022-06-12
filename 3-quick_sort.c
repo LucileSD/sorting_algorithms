ef14246bbf0f12d9927127891b5907c794af8998
LucileSD <4316@holbertonschool.com>
2022-06-12 12:40:44 +0000

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,19 +1,72 @@
 #include "sort.h"
 /**
- * @swap_array - swaps 2 elements
- * @a: first element 
+ * swap_array - swaps 2 elements
+ * @a: first element
  * @b: second element
  */
 void swap_array(int *a, int *b)
 {
-	int *tmp;
+	int tmp;
 
-	tmp = a;
-	a = b;
-	b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 
 }
 
+/**
+ * partition - find the partition position
+ * @array: the array
+ * @low: the first inex
+ * @high: the last index
+ * @size: the size if the array
+ * Return: i
+ */
+int partition(int array[], int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = (low);
+	int j;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			if (i != j)
+			{
+				swap_array(&array[i], &array[j]);
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
+	if (array[high] < array[i])
+	{
+		swap_array(&array[i], &array[high]);
+		print_array(array, size);
+	}
+	return (i);
+}
+
+/**
+ * first_quicksort - quicksort to begin the algorithm
+ * @array: the array
+ * @low: first element in the array
+ * @high: last element of the array
+ * @size: the size of the array
+ * Return: None
+ */
+void first_quicksort(int *array, int low, int high, size_t size)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, low, high, size);
+		first_quicksort(array, low, pivot - 1, size);
+		first_quicksort(array, pivot + 1, high, size);
+	}
+}
 /**
  * quick_sort - sorts an array of integers in ascending
  * order using the Quick sort algorithm
@@ -23,30 +76,11 @@ void swap_array(int *a, int *b)
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t pivot;
-	size_t idx = 0, end = size - 1, new_idx, right_size = 0, left_size = 0;
+	int low = 0;
+	int high = (size - 1);
 
 	if (!array || size < 2)
 		return;
 
-	if (idx < end)
-		pivot = idx;
-	while (idx < end)
-	{
-		while (array[idx] <= array[end])
-			idx++;
-		while (array[end] > array[pivot])
-			end--;
-		if (idx < end)
-			swap_array(&array[idx], &array[end]);
-	}
-	swap_array(&array[pivot], &array[end]);
-
-	for (new_idx = idx; new_idx < pivot; new_idx++)
-		right_size++;
-	quick_sort(array, right_size);
-
-	for (new_idx = pivot + 1; new_idx < end; new_idx++)
-		left_size++;
-	quick_sort(array, left_size);
+	first_quicksort(array, low, high, size);
 }
--- sort.h
+++ sort.h
@@ -25,5 +25,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap_array(int *a, int *b);
+void first_quicksort(int *array, int low, int high, size_t size);
+int partition(int array[], int low, int high, size_t size);
 
 #endif
