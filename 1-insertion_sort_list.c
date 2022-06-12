ce72230aaedb6f52a85bea4385326135505b5cd1
LucileSD <4316@holbertonschool.com>
2022-06-12 10:33:17 +0000

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,30 +1,30 @@
 #include "sort.h"
 /**
  * swap - swaps in a doubly linked list
- * @previous: prev nod of current
+ * @previous: prev node of current
  * @current: the current node
- * @list: the head of the list 
+ * @list: the head of the list
  * Return: None
  */
-void swap(listint_t *previous, listint_t *current, listint_t *list)
+void swap(listint_t *current, listint_t *previous, listint_t **list)
 {
-	listint_t *tmp = previous->prev;
+	listint_t *tmp;
 
-	if (!tmp)
-		list = current;
-	
-	else
-	{
-		(void)list;
+	tmp = previous->prev;
+	if (tmp)
 		tmp->next = current;
-		current->prev = tmp;
-		previous->prev = current;
-		previous->next = current->next;
-		current->next = previous;
-		
-		if (previous->next)
-			previous->next->prev = previous;
-	}
+	else
+		*list = current;
+
+	current->prev = tmp;
+
+	previous->prev = current;
+	previous->next = current->next;
+	current->next = previous;
+	if (previous->next)
+		previous->next->prev = previous;
+	else
+		previous->next = NULL;
 }
 
 /**
@@ -35,17 +35,19 @@ void swap(listint_t *previous, listint_t *current, listint_t *list)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *browse = (*list);
+	listint_t *browse = NULL;
 
-	if (*list)
+	if (!*list || !list)
+		return;
+
+	browse = (*list)->next;
+	while (browse)
 	{
-		while (browse->next)
+		while (browse->prev && browse->prev->n > browse->n)
 		{
-			if (browse->prev->n > browse->n)
-			{
-				swap(browse->prev, browse, *list);
-				print_list(list);
-			}
+			swap(browse, browse->prev, list);
+			print_list(*list);
 		}
+		browse = browse->next;
 	}
 }
