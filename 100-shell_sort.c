a9fde616126bf703acdefec414c7b6de31d7b2e3
LucileSD <4316@holbertonschool.com>
2022-06-13 12:13:46 +0000

    interval

---
+++ 100-shell_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+/**
+ * swap_array - swaps 2 elements
+ * @a: first element
+ * @b: second element
+ */
+void swap_array(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+
+}
+
+/**
+ * shell_sort -  sorts an array of integers in ascending order using the Shell
+ * sort algorithm, using the Knuth sequence
+ * @array: the array
+ * @size: the size of the array
+ * Return: None
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t first, second;
+	size_t interval = 1;
+
+	while (interval * 3 + 1 < size)
+		interval = interval * 3 + 1;
+
+	while (interval > 0)
+	{
+		for (first = 0; first < size; first++)
+		{
+			for (second = first + interval; second < size; second++)
+			{
+				if (array[first] > array[second])
+					swap_array(&array[first], &array[second]);
+			}
+		}
+		print_array(array, size);
+		interval = (interval - 1) / 3;
+	}
+}
